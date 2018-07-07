09e10e008f1ed8c5c900150071bc15af47951ac0
Nepryahin Daniil <nervoidaz@yandex.ru>
2018-07-07 22:41:20 +0300

    Added save/load functions for data

--- ih_functions.py
+++ ih_functions.py
@@ -7,6 +7,34 @@ import pickle
 import os.path
 
 
+def load_data(file_name):
+    """
+    load data from file
+
+    :param file_name: file name
+    :return: data: dictionary with 'api' and 'follows' keys
+    """
+    data = None
+    if os.path.exists(file_name):
+        f = open(file_name, 'br')
+        data = pickle.load(f)
+        f.close()
+    return data
+
+
+def save_data(file_name, data):
+    """
+    save data to file
+
+    :param file_name: file name
+    :param data: data to save
+    :return: None
+    """
+    f = open(file_name, 'bw')
+    pickle.dump(data, f)
+    f.close()
+
+
 def open_api(username, password):
     """
     open api. Load data from file if exists or create new file and data
@@ -17,18 +45,15 @@ def open_api(username, password):
              api is InstagramApi
              follows is array of users that you've already follows.
     """
-    if os.path.exists(username.lower() + '.api'):
-        f = open(username.lower() + '.api', 'br')
-        data = pickle.load(f)
-        f.close()
+    file_name = username.lower() + '.api'
+    if os.path.exists(file_name):
+        data = load_data(file_name)
     else:
         api = InstagramAPI(username=username, password=password)
         follows = []
         api.login()
         data = {'api': api, 'follows': follows}
-        f = open(username.lower() + '.api', 'bw')
-        pickle.dump(data, f)
-        f.close()
+        save_data(file_name, data)
     return data
 
 
@@ -37,6 +62,7 @@ def unfollow_not_followers(api, delete=True):
     Unfollow useras that don't follow you
 
     :param api: InstagramApi
+    :param delete: flag to delete or not unfollowers
     :return: not_follows: array of usernames that don't follow you
     """
     followers = api.getTotalSelfFollowers()
