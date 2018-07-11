5ffa715d8fa3f43c6a5a2c10f55071fd37ea37f4
Nepryahin Daniil <nervoidaz@yandex.ru>
2018-07-11 21:12:47 +0300

    Added some preview examples

--- insta_helper.py
+++ insta_helper.py
@@ -22,12 +22,33 @@ else:
     password = input("Password [> ")
     data = ih.open_api(username, password)
 
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# Unfollow not followers
+
 # not_follows = ih.unfollow_not_followers(data['api'])
 # print(not_follows)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# Your estimation
+
 # estimation = ih.user_estimate(data['api'], data['api'].username_id)
 # print(estimation)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# Follow and like new users
+
 followings = data['api'].getTotalSelfFollowings()
 user = random.choice(followings)
 print(user['username'])
-res = ih.user_followers_like_follow_helper(data, user['pk'], 10)
+res = ih.user_followers_like_follow_helper(data, user['pk'], 15, 10, 1,
+                                           (0.6, 1.1))
 print(res)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# Update not follow again list
+
+#follows = ih.init_no_need_to_follow_list(data['api'])
+#for f in follows:
+#    if f not in data['follows']:
+#        data['follows'].append(f)
+#ih.save_data(data['api'].username.lower() + '.api', data)
